a72699516c2147be73694fe2725ea83027e9be41
Almost42 <crsl9299@qq.com>
2025-03-20 18:01:45 +0800

    增加PropTypes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,44 @@
+import PropTypes from "prop-types";
 import React from "react";
 import ReactDOM from 'react-dom/client'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
+
+function Son(props) {
+    const colorArr = props.colors;
+    const list = colorArr.map((item, index) => {
+        return <li key={index}>{item}</li>
+    });
+    return (
+        <ul>            {list}        </ul>
+    )
+}
+Son.propTypes = {
+    colors: PropTypes.array
+}
+
+class Parent extends React.Component {
+    static propTypes = {
+        currentTime: PropTypes.number.isRequired // 确保 currentTime 是必填的数字
+    }
+
+    currentTime = this.props.currentTime;
+    colors = ["red", "blue"];
+
+    render() {
+        return (
+            <div>
+                <Son colors={this.colors} />
+            </div>
+        );
+    }
+}
+
+
+
 root.render(
     <>
-
+        <Parent currentTime={Date.now()} /> 
     </>
 )
